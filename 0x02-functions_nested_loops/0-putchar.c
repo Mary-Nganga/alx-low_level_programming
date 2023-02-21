3a32be1b72f849ed0d118e3a6f583dc877d5b930
Mary-Nganga <ngangawambui88@gmail.com>
2023-02-21 07:43:16 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	int i;
-	char c[] = "_putchar";
-
-        for (i=0; c[i];)
-	{
-	_putchar (c[i]);
-	 i++;
-	}	
+	_putchar;
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
