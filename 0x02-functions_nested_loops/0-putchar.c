fe3eb1aa38d9e5af52007e3ceebf2d8626635ba7
Mary-Nganga <ngangawambui88@gmail.com>
2023-02-21 02:47:12 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.c>
+/**
+ * main - prints _putchar
+ *
+ * Return 0 if successful
+ */
+int main(void)
+{
+	char str[] = "_putchar"
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
