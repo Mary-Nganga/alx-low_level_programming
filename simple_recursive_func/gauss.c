2a631cfb414b50e3b8401056568aaf652485caf3
Mary-Nganga <ngangawambui88@gmail.com>
2023-03-31 00:04:29 -0700

    function that add 1 to n

---
+++ simple_recursive_func/gauss.c
@@ -0,0 +1,15 @@
+// Mary Nganga
+// 31 march 2023
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * add up 1 to n
+  */
+int main (int argc, char *argv[])
+{
+	int n;
+	printf ("Enter a number\n");
+	scanf ("%d",&n);
+	return (0);
+}
